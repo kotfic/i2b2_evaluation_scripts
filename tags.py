b5e99fca91e0b5c0d43b9f2ff2d7ea77fbf3d3d6
Christopher Kotfila <kotfic@gmail.com>
2014-07-11 13:20:16 -0400

    Fixing warning edge case
    Warning in tags.py assumes there is an id attribute,  but id attribute
    is not generally required by any tags.

--- tags.py
+++ tags.py
@@ -180,14 +180,16 @@ class AnnotatorTag(Tag):
                     fstr = "WARNING: Expected attribute '{}' for xml element "
                     fstr += "<{} ({})>  was not valid ('{}')"
                     print(fstr.format(k, element.tag,
-                                      element.attrib['id'],
+                                      element.attrib['id'] 
+                                      if 'id' in element.attrib.keys() else '',
                                       element.attrib[k]))
                     setattr(self, k, element.attrib[k])
 
             elif k in self.key:
                 fstr = "WARNING: Expected attribute '%s' for xml element "
                 fstr += "<%s ('%s')>, setting to ''"
-                print(fstr.format(k, element.tag, element.attrib['id']))
+                print(fstr.format(k, element.tag, element.attrib['id'] 
+                                  if 'id' in element.attrib.keys() else ''))
 
                 setattr(self, k, '')
 
