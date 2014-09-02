e08db161391200f4520336840c8f762515a4243b
Christopher Kotfila <kotfic@gmail.com>
2014-09-02 16:38:08 -0400

    moving into setuptools style install

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="i2b2eval",
+    author="Christoper Kotfila",
+    author_email="ckotfila@albany.edu",
+    license="APACHE",
+    description="A package for evaluating Standoff Annotations in the i2b2 2014 shared task",
+    version="1.2.1",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            'i2b2eval = i2b2eval.evaluate:main'
+        ]
+    }
+)
